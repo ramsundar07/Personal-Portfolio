48668c77e1590e98f560389de706c5ac256b8d76
ramsundar07 <ramjuly2002@gmail.com>
2023-01-26 20:21:01 +0530

    Added Favicon

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-
-var counter = 1;
-    setInterval(function(){
-      document.getElementById('radio' + counter).checked = true;
-      counter++;
-      if(counter > 8){
-        counter = 1;
-      }
-    }, 5000);
-
-
 function reveal() {
 
   var reveals = document.querySelectorAll(".reveal");
